1a416f1ea9e60b13e71ebd32d69a6c99928d8c34
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2024-07-12 17:11:49 +0200

    ok

--- server/src/models/chat.js
+++ server/src/models/chat.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose;
 const customFieldSchema = new Schema({
   name: { type: String, required: true },
   type: { type: String, required: true },
-  description: {type: String, required: true},
+  description: {type: String},
   value: Schema.Types.Mixed
 });
 
